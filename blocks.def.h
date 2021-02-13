2d53c81f476d1864c9674ab173713f048e876463
ixp123 <77308732+ixp123@users.noreply.github.com>
2021-02-13 18:14:04 -0500

    C1: My build of dwmblocks

---
+++ blocks.def.h
@@ -0,0 +1,15 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	//{"RAM:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+
+	//{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{" ", "~/github/dwmblocks/scripts/kernel",			360,		2},
+	{" ", "~/github/dwmblocks/scripts/memory",			  6,		1},
+	//{"VOL: ", "~/github/dwmblocks/scripts/volume", 			  2,	       10},
+
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
